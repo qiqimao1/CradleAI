30aae5d98341041adc812f8f87908e0add32758a
AliceSyndrome285 <943113638@qq.com>
2025-07-24 23:11:03 +0800

    update：gemini model names

--- app/pages/api-settings.tsx
+++ app/pages/api-settings.tsx
@@ -268,9 +268,8 @@ const ApiSettings = () => {
   const [modelPickerType, setModelPickerType] = useState<'primary' | 'backup'>('primary');
 
   const availableGeminiModels = [
-    'gemini-2.5-pro-exp-03-25',
-    'gemini-2.5-pro-preview-05-06',
-    'gemini-2.5-flash-preview-04-17', // 新增
+    'gemini-2.5-pro',
+    'gemini-2.5-flash', 
     'gemini-2.0-flash-exp',
     'gemini-2.0-pro-exp-02-05',
     'gemini-exp-1206',
